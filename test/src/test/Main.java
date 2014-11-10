6d4ba0240ab898c97aaa7d2b1016b6993fc8ac0a
diggo16 <daniell_94@hotmail.se>
2014-11-10 02:05:39 +0100

    add text

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 	System.out.println(string());
-	System.out.println("\n");
+	System.out.println("test");
 
 	}
 	public static  String string() {
